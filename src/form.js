05f514a0982d46dde1312013a38069936b8c5e85
Hyerin Lee <hyerynn0521@gmail.com>
2022-02-23 15:53:13 -0800

    everything together

--- src/form.js
+++ src/form.js
@@ -71,12 +71,13 @@ class Form extends React.Component {
   }
 } 
 
-ReactDOM.render(
+/*ReactDOM.render(
   <Form />,
   document.getElementById('root')
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export default Form;
