3ebae9f82424c1bc5a655f694905d16fdc6b63c7
PavanaAtawale <pavana.atawale@gmail.com>
2022-02-23 17:19:24 -0800

    separated Search into a different file

---
+++ src/Search.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import SearchBar from './SearchBar';
+import Announcer from './announcer';
+
+const posts = [
+    { id: '1', name: 'This first post is about React' },
+    { id: '2', name: 'This next post is about Preact' },
+    { id: '3', name: 'We have yet another React post!' },
+    { id: '4', name: 'This is the fourth and final post' },
+];
+
+const filterPosts = (posts, query) => {
+    if (!query) {
+        return posts;
+    }
+
+    return posts.filter((post) => {
+        const postName = post.name.toLowerCase();
+        return postName.includes(query);
+    });
+};
+
+const Search = () => {
+
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+    const filteredPosts = filterPosts(posts, searchQuery);
+
+    return (
+	<div>
+       <Announcer message={`${filteredPosts.length} posts`} />
+       <SearchBar
+		searchQuery={searchQuery}
+                 setSearchQuery={setSearchQuery}
+       />
+
+       <ul>
+       	{filteredPosts.map((post) => (
+         	<li key={post.id}>{post.name}</li>
+        	))}
+       </ul>
+</div>
+    
+    );
+
+}
+
+export default Search;
