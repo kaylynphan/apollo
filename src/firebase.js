51f3ac26b7ff7650139b43440376614df76f688c
Hyerin Lee <hyerynn0521@gmail.com>
2022-02-23 16:31:21 -0800

    setup firebase

--- src/firebase.js
+++ src/firebase.js
@@ -22,5 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth = getAuth(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app)
+export const provider = new GoogleAuthProvider();
