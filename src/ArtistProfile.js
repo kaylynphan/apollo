88eef4c0e0c71b9771331c50841ef78bdc20a748
Hyerin Lee <hyerynn0521@gmail.com>
2022-03-01 13:18:17 -0800

    pass data that was clicked on home page into artist page

--- src/ArtistProfile.js
+++ src/ArtistProfile.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './ArtistProfile.css';
 import albums from "./albums.json"
+import {useLocation} from 'react-router-dom'
+import { render } from '@testing-library/react';
 
-const posts = albums;
-class ArtistProfile extends React.Component {
-    
-    render() {
+const ArtistProfile = () => {
+    const location = useLocation();
+    const artist = location.state.artist;
+   
         return (
             <div className="artistprofile">
                 <h1></h1>
                 <div>
-            <h3 >Artist: {posts.artist}</h3>
+            <h3 >Artist: {artist}</h3>
                     <p>Here we would display reviews for this artist's albums.</p>
                 </div>
             </div>
         )
-    }
+    
+            
+        
+    
+    
 }
+
+/*class ArtistProfile extends React.Component {
+    
+    render() {
+        
+    }
+}*/
 
 export default ArtistProfile;
