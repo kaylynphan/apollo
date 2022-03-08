fb10fb9840fe3e7bfc3c19983d7eec205d6b8a25
kaylynphan <kaylynkhowong@gmail.com>
2022-03-08 14:09:51 -0800

    login feature working

--- src/ReviewForm.js
+++ src/ReviewForm.js
@@ -83,7 +83,7 @@ function ReviewForm(props) {
             rating: value,
             album: album, 
             review: review, 
-            user: "USERNAME",
+            user: auth.currentUser.displayName,
         });
         props.handleSubmissions();
     }
