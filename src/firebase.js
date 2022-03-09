0fe5aae8d7f1e1532dfc7d960f3874fe7794189a
Hyerin Lee <hyerynn0521@gmail.com>
2022-03-08 22:14:13 -0800

    code cleanup in firebase.js, indicate when not logged in on submit button

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
-//import firebase.getApp from 'firebase/app';
 
 
 // TODO: Add SDKs for Firebase products that you want to use
