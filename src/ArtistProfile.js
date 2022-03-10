cfa9bbe7f8261eb218c364bf15e7ea877b4d68fc
kaylynphan <kaylynkhowong@gmail.com>
2022-03-09 22:51:26 -0800

    album cards in artist bio

--- src/ArtistProfile.js
+++ src/ArtistProfile.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './ArtistProfile.css';
 import {useLocation} from 'react-router-dom'
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const ArtistProfile = () => {
     const location = useLocation();
@@ -12,24 +15,41 @@ const ArtistProfile = () => {
     const albumlist = Object.keys(albums)
     const bio = "This artist is a person. Born on this day. Became famous blah blah blah"
     const url = "https://www.weact.org/wp-content/uploads/2016/10/Blank-profile.png";
-    let str = ""
+    //let str = ""
+    console.log(albums);
     const AlbumGrid = [];
     for(let element in albumlist){
         AlbumGrid.push(
-            <Grid item xs={6}>
+    
+                <Card sx={{ width: 200, display: 'flex', flexDirection: 'column', padding: 1 }}>
+                    <Typography variant="h6" textAlign="center">{albumlist[element]}</Typography>
+                    <CardMedia
+                        compontent="img"
+                        sx={{ height: 200, paddingTop: 2 }}
+                        image={albums[albumlist[element]].imgURL} 
+                        alt={albumlist[element]}
+                    />
+                </Card>
+                
+                /*
                 <div className = "album">
                     <h4>{albumlist[element]}</h4>
                     <img src={albums[albumlist[element]].imgURL} alt={albumlist[element]}/>
                 </div>
-            </Grid> )
+                */
+                
+            )
     }
 
+    /*
     for(let element in albumlist){
         str += albumlist[element];
         if (element < albumlist.length - 1){
             str += ", "
         }
     }
+    */
+    
         
 
     return (
@@ -39,12 +59,14 @@ const ArtistProfile = () => {
                 <p>Here we would display reviews for {artist}'s albums.</p>
                 <h4 className="bio">Bio: {bio}</h4>
                 <h3>Here is a list of all {artist}'s albums</h3>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={2}>
+                
+                <Grid container sx={{ justifyContent: 'center'}}>
+                    <Grid item sx={{ width: 880, columns: 4, justifyContent: 'space-between'}}>
                         {AlbumGrid}
                     </Grid>
-                    
-                </Box>
+                </Grid>
+                
+                
             </div>
     )
     
